2929deef17377f52b6b6e30640259d1b8e348431
Kai.Xiong <xiongkai@petalmail.com>
2025-01-10 19:23:53 +0800

    编辑

---
+++ lib/utilities/utils.dart
@@ -0,0 +1,17 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:musify/utilities/common_variables.dart';
+
+BorderRadius getItemBorderRadius(int index, int totalLength) {
+  const defaultRadius = BorderRadius.zero;
+  if (totalLength == 1) {
+    return commonCustomBarRadius; // Only one item
+  } else if (index == 0) {
+    return commonCustomBarRadiusFirst; // First item
+  } else if (index == totalLength - 1) {
+    return commonCustomBarRadiusLast; // Last item
+  }
+  return defaultRadius; // Default for middle items
+}
