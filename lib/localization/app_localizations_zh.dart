0648cc5089640d124f2dc816ba33f53a73499699
Kai.Xiong <xiongkai@petalmail.com>
2026-06-03 09:52:41 +0800

    fix: 更新中文翻译以提高一致性和准确性

--- lib/localization/app_localizations_zh.dart
+++ lib/localization/app_localizations_zh.dart
@@ -2,7 +2,7 @@ import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
 
-/// The translations for Chinese (`zh`).
+/// The translations for Chinese (Simplified) (`zh`).
 class AppLocalizationsZh extends AppLocalizations {
   AppLocalizationsZh([String locale = 'zh']) : super(locale);
 
@@ -10,22 +10,22 @@ class AppLocalizationsZh extends AppLocalizations {
   String get about => '关于';
 
   @override
-  String get accentChangeMsg => '强调色已成功更新';
+  String get accentChangeMsg => '主题色更新成功';
 
   @override
-  String get accentColor => '强调色';
+  String get accentColor => '主题色';
 
   @override
   String get add => '添加';
 
   @override
-  String get addToLikedPlaylists => '添加到喜欢的播放列表';
+  String get addToLikedPlaylists => '添加到喜欢的歌单';
 
   @override
   String get addToLikedSongs => '添加到喜欢的歌曲';
 
   @override
-  String get addToPlaylist => '添加到播放列表';
+  String get addToPlaylist => '添加到歌单';
 
   @override
   String get addedSuccess => '添加成功';
@@ -37,7 +37,7 @@ class AppLocalizationsZh extends AppLocalizations {
   String get albums => '专辑';
 
   @override
-  String get appUpdateIsAvailable => '有可用更新';
+  String get appUpdateIsAvailable => '发现新版本';
 
   @override
   String get artist => '艺术家';
@@ -49,13 +49,13 @@ class AppLocalizationsZh extends AppLocalizations {
   String get audioQualityMsg => '音质已更新';
 
   @override
-  String get automaticSongPicker => '自动选歌器';
+  String get automaticSongPicker => '自动歌曲选择器';
 
   @override
   String get backedupSuccess => '数据备份成功';
 
   @override
-  String get backupError => '备份数据时出错';
+  String get backupError => '备份数据时发生错误';
 
   @override
   String get backupUserData => '备份用户数据';
@@ -64,7 +64,7 @@ class AppLocalizationsZh extends AppLocalizations {
   String get becomeSponsor => '成为赞助者';
 
   @override
-  String get cacheMsg => '缓存已清理';
+  String get cacheMsg => '缓存已清除';
 
   @override
   String get cancel => '取消';
@@ -73,19 +73,19 @@ class AppLocalizationsZh extends AppLocalizations {
   String get chooseBackupDir => '选择备份文件夹';
 
   @override
-  String get chooseBackupFiles => '选择备份文件';
+  String get chooseBackupFiles => '选择要备份的文件';
 
   @override
   String get clear => '清除';
 
   @override
-  String get clearCache => '清理缓存';
+  String get clearCache => '清除缓存';
 
   @override
   String get clearRecentlyPlayed => '清除最近播放';
 
   @override
-  String get clearRecentlyPlayedQuestion => '确定要清除最近播放历史吗？';
+  String get clearRecentlyPlayedQuestion => '确定要清除最近播放记录吗？';
 
   @override
   String get clearSearchHistory => '清除搜索历史';
@@ -103,19 +103,19 @@ class AppLocalizationsZh extends AppLocalizations {
   String get copyLogs => '复制日志';
 
   @override
-  String get copyLogsNoLogs => '没有日志可复制';
+  String get copyLogsNoLogs => '没有可复制的日志';
 
   @override
   String get copyLogsSuccess => '日志复制成功';
 
   @override
-  String get customPlaylistImgUrl => '自定义播放列表图片 URL';
+  String get customPlaylistImgUrl => '自定义歌单图片链接';
 
   @override
-  String get customPlaylistName => '自定义播放列表名称';
+  String get customPlaylistName => '自定义歌单名称';
 
   @override
-  String get deletePlaylist => '删除播放列表';
+  String get deletePlaylist => '删除歌单';
 
   @override
   String get download => '下载';
@@ -124,34 +124,34 @@ class AppLocalizationsZh extends AppLocalizations {
   String get downloadAppUpdate => '下载应用更新';
 
   @override
-  String get dynamicColor => '动态强调色（Android 12+）';
+  String get dynamicColor => '动态主题色（Android 12+）';
 
   @override
   String get error => '发生错误，请重试。';
 
   @override
-  String get folderRestrictions => '由于 Android 限制，需要选择特定文件夹（如 Documents 或 Downloads）进行备份。';
+  String get folderRestrictions => '由于 Android 限制，需要选择特定文件夹（如文档或下载）进行备份。';
 
   @override
   String get home => '首页';
 
   @override
-  String get invalidYouTubePlaylist => '无效的 YouTube 播放列表';
+  String get invalidYouTubePlaylist => '无效的 YouTube 歌单';
 
   @override
   String get language => '语言';
 
   @override
-  String get languageMsg => '语言已成功更新';
+  String get languageMsg => '语言更新成功';
 
   @override
-  String get library => '媒体库';
+  String get library => '资料库';
 
   @override
-  String get licenses => '许可证';
+  String get licenses => '开源许可';
 
   @override
-  String get likedPlaylists => '喜欢的播放列表';
+  String get likedPlaylists => '喜欢的歌单';
 
   @override
   String get likedSongs => '喜欢的歌曲';
@@ -160,22 +160,22 @@ class AppLocalizationsZh extends AppLocalizations {
   String get lyrics => '歌词';
 
   @override
-  String get lyricsNotAvailable => '歌词不可用';
+  String get lyricsNotAvailable => '暂无歌词';
 
   @override
-  String get makeOffline => '离线可用';
+  String get makeOffline => '保存到本地';
 
   @override
   String get name => '名称';
 
   @override
-  String get newAnnouncement => '新公告已发布';
+  String get newAnnouncement => '有新公告发布';
 
   @override
-  String get noCustomPlaylists => '尚未创建自定义播放列表';
+  String get noCustomPlaylists => '暂无自定义歌单';
 
   @override
-  String get notYTlist => '无效的 YouTube 播放列表 ID';
+  String get notYTlist => '无效的 YouTube 歌单 ID';
 
   @override
   String get offlineMode => '离线模式';
@@ -184,31 +184,31 @@ class AppLocalizationsZh extends AppLocalizations {
   String get offlineSongs => '离线歌曲';
 
   @override
-  String get originalRecommendations => '原创推荐算法';
+  String get originalRecommendations => '原始推荐算法';
 
   @override
   String get others => '其他';
 
   @override
-  String get playlist => '播放列表';
+  String get playlist => '歌单';
 
   @override
-  String get playlistAlreadyExists => '播放列表已存在';
+  String get playlistAlreadyExists => '歌单已存在';
 
   @override
-  String get playlistUpdated => '播放列表已成功更新';
+  String get playlistUpdated => '歌单更新成功';
 
   @override
-  String get playlists => '播放列表';
+  String get playlists => '歌单';
 
   @override
-  String get predictiveBack => '预测性返回（Android 14+）';
+  String get predictiveBack => '预测性返回手势（Android 14+）';
 
   @override
   String get preferences => '偏好设置';
 
   @override
-  String get provideIdOrNameError => '请提供 YouTube ID 或自定义播放列表名称';
+  String get provideIdOrNameError => '请提供 YouTube ID 或自定义歌单名称';
 
   @override
   String get pureBlackTheme => '纯黑主题';
@@ -217,7 +217,7 @@ class AppLocalizationsZh extends AppLocalizations {
   String get recentlyPlayed => '最近播放';
 
   @override
-  String get recentlyPlayedMsg => '最近播放历史已清除';
+  String get recentlyPlayedMsg => '最近播放记录已清除';
 
   @override
   String get recommendedForYou => '为你推荐';
@@ -226,25 +226,25 @@ class AppLocalizationsZh extends AppLocalizations {
   String get remove => '移除';
 
   @override
-  String get removeFromLikedPlaylists => '从喜欢的播放列表中移除';
+  String get removeFromLikedPlaylists => '从喜欢的歌单中移除';
 
   @override
   String get removeFromLikedSongs => '从喜欢的歌曲中移除';
 
   @override
-  String get removeFromPlaylist => '从播放列表中移除';
+  String get removeFromPlaylist => '从歌单中移除';
 
   @override
-  String get removeOffline => '从离线中移除';
+  String get removeOffline => '从本地移除';
 
   @override
-  String get removePlaylistQuestion => '确定要移除此播放列表吗？';
+  String get removePlaylistQuestion => '确定要删除这个歌单吗？';
 
   @override
-  String get removeSearchQueryQuestion => '确定要移除此搜索记录吗？';
+  String get removeSearchQueryQuestion => '确定要删除这个搜索记录吗？';
 
   @override
-  String get restartAppMsg => '重启应用以应用更改';
+  String get restartAppMsg => '重启应用以使更改生效';
 
   @override
   String get restoreError => '恢复数据时出错';
@@ -262,7 +262,7 @@ class AppLocalizationsZh extends AppLocalizations {
   String get searchHistoryMsg => '搜索历史已清除';
 
   @override
-  String get settingChangedMsg => '设置已成功更新';
+  String get settingChangedMsg => '设置更新成功';
 
   @override
   String get settings => '设置';
@@ -271,7 +271,7 @@ class AppLocalizationsZh extends AppLocalizations {
   String get songAdded => '歌曲已添加';
 
   @override
-  String get songAlreadyInPlaylist => '歌曲已在播放列表中';
+  String get songAlreadyInPlaylist => '歌曲已在歌单中';
 
   @override
   String get songRemoved => '歌曲已移除';
@@ -280,13 +280,13 @@ class AppLocalizationsZh extends AppLocalizations {
   String get songs => '歌曲';
 
   @override
-  String get sponsorProject => '赞助此项目';
+  String get sponsorProject => '赞助项目';
 
   @override
-  String get suggestedArtists => '推荐的艺术家';
+  String get suggestedArtists => '推荐艺术家';
 
   @override
-  String get suggestedPlaylists => '推荐的播放列表';
+  String get suggestedPlaylists => '推荐歌单';
 
   @override
   String get themeMode => '主题模式';
@@ -295,17 +295,17 @@ class AppLocalizationsZh extends AppLocalizations {
   String get tools => '工具';
 
   @override
-  String get understand => '我明白';
+  String get understand => '我知道了';
 
   @override
   String get undo => '撤销';
 
   @override
-  String get useSquigglySlider => '使用波浪滑块';
+  String get useSquigglySlider => '波浪形滑块';
 
   @override
-  String get userPlaylists => '用户播放列表';
+  String get userPlaylists => '我的歌单';
 
   @override
-  String get youtubePlaylistLinkOrId => 'YouTube 播放列表链接或 ID';
+  String get youtubePlaylistLinkOrId => 'YouTube 歌单链接或 ID';
 }
